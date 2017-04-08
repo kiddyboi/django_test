3620e10ba02e9eadf7548790f235670ee1c2b735
kiddyboi <Cody@Chaus-MacBook-Pro.local>
2017-04-08 20:30:56 +0900

    Added with Jinja Templating

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^webapp/', include('webapp.urls'))
+    url(r'^webapp/', include('webapp.urls')),
+    url(r'^personal/', include('personal.urls'))
 ]
