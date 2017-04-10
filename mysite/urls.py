c02ae0da7209555794b9d5e4cc69ac2c3b97f804
kiddyboi <cody_chau@slj.local>
2017-04-10 11:50:40 +0900

    Added Contact Template with Additional rendering

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url, include
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^webapp/', include('webapp.urls')),
-    url(r'^personal/', include('personal.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('personal.urls')),
 ]
